7e3566a22251ab99c510f687c19a86ffa441435f
its-kumar-yash <its.yash.kumar23@gmail.com>
2023-06-12 18:31:04 +0530

    added more styling

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,8 +5,16 @@ const Calculator = () => {
   const [calculatedResult, setCalculatedResult] = useState("");
 
   const clickHandler = (event) => {
-    setCalculatedResult(event.target.name);
+    setCalculatedResult(calculatedResult.concat(event.target.value));
     console.log(calculatedResult);
+  }
+
+  const clearHandler = () => {
+    setCalculatedResult("");
+  }
+
+  const backspaceHandler = () => {
+    setCalculatedResult(calculatedResult.slice(0, calculatedResult.length - 1));
   }
 
   return (
@@ -16,24 +24,24 @@ const Calculator = () => {
           <input type="text" value={calculatedResult} />
         </form>
         <div className="keypad">
-          <button onClick={clickHandler} >Clear</button>
-          <button onClick={clickHandler} >C</button>
-          <button onClick={clickHandler} >&divide;</button>
-          <button onClick={clickHandler} >7</button>
-          <button onClick={clickHandler} >8</button>
-          <button onClick={clickHandler} >9</button>
-          <button onClick={clickHandler} >&times;</button>
-          <button onClick={clickHandler} >4</button>
-          <button onClick={clickHandler} >5</button>
-          <button onClick={clickHandler} >6</button>
-          <button onClick={clickHandler} >&ndash;</button>
-          <button onClick={clickHandler} >1</button>
-          <button onClick={clickHandler} >2</button>
-          <button onClick={clickHandler} >3</button>
-          <button onClick={clickHandler} >+</button>
-          <button onClick={clickHandler} >0</button>
-          <button onClick={clickHandler} >.</button>
-          <button onClick={clickHandler} >=</button>
+          <button onClick={clearHandler} id="clear" className="highlight">Clear</button>
+          <button onClick={backspaceHandler} id="backspace" className="highlight">C</button>
+          <button onClick={clickHandler} value="/" className="highlight">&divide;</button>
+          <button onClick={clickHandler} value="7">7</button>
+          <button onClick={clickHandler} value="8">8</button>
+          <button onClick={clickHandler} value="9">9</button>
+          <button onClick={clickHandler} value="*" className="highlight">&times;</button>
+          <button onClick={clickHandler} value="4">4</button>
+          <button onClick={clickHandler} value="5">5</button>
+          <button onClick={clickHandler} value="6">6</button>
+          <button onClick={clickHandler} value="-" className="highlight">&ndash;</button>
+          <button onClick={clickHandler} value="1">1</button>
+          <button onClick={clickHandler} value="2">2</button>
+          <button onClick={clickHandler} value="3">3</button>
+          <button onClick={clickHandler} value="+" className="highlight">+</button>
+          <button onClick={clickHandler} value="0">0</button>
+          <button onClick={clickHandler} value=".">.</button>
+          <button onClick={clickHandler} id="equal" className="highlight">=</button>
         </div>
       </div>
     </React.Fragment>
