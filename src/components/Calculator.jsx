4ddb17d1364f385b691355294a4c3fc8f60ec6a4
its-kumar-yash <its.yash.kumar23@gmail.com>
2023-06-12 18:08:07 +0530

    added basic styling to the keypad

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,74 +1,40 @@
 import React, { useEffect, useState } from "react";
-import styles from "./Calculator.module.css";
+import "./Calculator.css";
 
 const Calculator = () => {
-  //calculator
-  const [inputNumber, setInputNumber] = useState(0);
-  const [displayResult, setDisplayResult] = useState(0);
-  const [isDecimal, setIsDecimal] = useState(false);
-  const [decimalCount, setDecimalCount] = useState(1);
-  const [operator, setOperator] = useState("");
-  const [calculatedResult, setCalculatedResult] = useState(0);
+  const [calculatedResult, setCalculatedResult] = useState("");
 
-  //re-render the app when the input is changed
-  useEffect(() => {
-    setDisplayResult(inputNumber);
-  }, [inputNumber]);
-
-  //re-render the app when the calculation is changed
-  useEffect(() => {
-    setDisplayResult(calculatedResult);
-  }, [calculatedResult]);
-
-  //receive render from input button
-  const inputNumberTotal = (num) => {
-    //when the input number is in decimal
-    if (isDecimal) {
-      num = num / Math.pow(10, decimalCount);
-      setDecimalCount(decimalCount + 1);
-      //Returns a string representing a number in fixed-point notation.
-      setInputNumber(parseFloat((inputNumber + num).toFixed(decimalCount)));
-      console.log(inputNumber);
-    } else {
-      setInputNumber(inputNumber * 10 + num);
-      console.log(inputNumber);
-    }
-  };
-
-  //receive operator from input button
-  const inputOperator = (operator) => {
-    setOperator(operator);
-    console.log(operator);
-    setInputNumber(0);
-  };
+  const clickHandler = (event) => {
+    setCalculatedResult(event.target.name);
+    console.log(calculatedResult);
+  }
 
   return (
     <React.Fragment>
-      <div>
-        <section className={styles.monitor}>
-          <p>Display</p>
-        </section>
-        <section className={styles.calbtnContainer}>
-          <button>AC</button>
-          <button>-/+</button>
-          <button>%</button>
-          <button onClick={() => inputOperator("/")}>/</button>
-          <button onClick={() => inputNumberTotal(7)}>7</button>
-          <button onClick={() => inputNumberTotal(8)}>8</button>
-          <button onClick={() => inputNumberTotal(9)}>9</button>
-          <button onClick={() => inputOperator("*")}>*</button>
-          <button onClick={() => inputNumberTotal(4)}>4</button>
-          <button onClick={() => inputNumberTotal(5)}>5</button>
-          <button onClick={() => inputNumberTotal(6)}>6</button>
-          <button onClick={() => inputOperator("-")}>-</button>
-          <button onClick={() => inputNumberTotal(1)}>1</button>
-          <button onClick={() => inputNumberTotal(2)}>2</button>
-          <button onClick={() => inputNumberTotal(3)}>3</button>
-          <button onClick={() => inputOperator("+")}>+</button>
-          <button onClick={() => inputNumberTotal(0)}>0</button>
-          <button onClick={() => setIsDecimal(true)}>.</button>
-          <button>=</button>
-        </section>
+      <div className="container">
+        <form>
+          <input type="text" value={calculatedResult} />
+        </form>
+        <div className="keypad">
+          <button onClick={clickHandler} >Clear</button>
+          <button onClick={clickHandler} >C</button>
+          <button onClick={clickHandler} >&divide;</button>
+          <button onClick={clickHandler} >7</button>
+          <button onClick={clickHandler} >8</button>
+          <button onClick={clickHandler} >9</button>
+          <button onClick={clickHandler} >&times;</button>
+          <button onClick={clickHandler} >4</button>
+          <button onClick={clickHandler} >5</button>
+          <button onClick={clickHandler} >6</button>
+          <button onClick={clickHandler} >&ndash;</button>
+          <button onClick={clickHandler} >1</button>
+          <button onClick={clickHandler} >2</button>
+          <button onClick={clickHandler} >3</button>
+          <button onClick={clickHandler} >+</button>
+          <button onClick={clickHandler} >0</button>
+          <button onClick={clickHandler} >.</button>
+          <button onClick={clickHandler} >=</button>
+        </div>
       </div>
     </React.Fragment>
   );
